8e6fcf2edd9665d5beacdfca9354679633f8367f
bluvar <0x317374@gmail.com>
2023-06-28 22:56:44 +0700

    chore(checkpoint): 28/06/2023, 15:56:44 GMT+0000

--- src/handler/query.ts
+++ src/handler/query.ts
@@ -4,6 +4,8 @@ import { encode } from "gpt-3-encoder";
 import { Document } from "langchain/document";
 import { forEach } from "lodash";
 
+const DEFAULT_MAX_K = 13;
+
 const queryHandler = async (req: Request, res: Response) => {
   const {
     doc_id: docId,
@@ -13,17 +15,23 @@ const queryHandler = async (req: Request, res: Response) => {
     maxTokens = 3072,
     maxScore = 0.45,
     includeAllIfKLessThanScore = 0.3,
+    scoreChangeThreshold = 0.02,
   } = req.body;
   const vectorStore = await getVectorStore(docId, apiKey);
   const results = await vectorStore.similaritySearchWithScore(
     query,
-    Math.max(k, 13)
+    Math.max(k, DEFAULT_MAX_K)
   );
   const data: [Document, number][] = [];
   const totalTokens = { current: 0 };
+  const lastScore = { current: -1 };
 
   forEach(results, (r) => {
     if (r[1] > maxScore) return false;
+
+    if (lastScore.current === -1) {
+      lastScore.current = r[1];
+    }
 
     const doc = r[0];
     const encoded = encode(doc.pageContent);
@@ -31,7 +39,11 @@ const queryHandler = async (req: Request, res: Response) => {
     totalTokens.current += encoded.length;
 
     if (totalTokens.current <= maxTokens) {
-      if (r[1] > includeAllIfKLessThanScore) {
+      const canAddC1 = r[1] <= includeAllIfKLessThanScore;
+      const canAddC2 = r[1] - lastScore.current <= scoreChangeThreshold;
+      const canAdd = canAddC1 || canAddC2;
+
+      if (!canAdd) {
         if (data.length >= k) {
           return false;
         }
