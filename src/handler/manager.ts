7444005632a6815356aeed3392b36b389185ed38
bluvar <0x317374@gmail.com>
2023-06-29 23:33:15 +0700

    chore(checkpoint): 29/06/2023, 16:33:15 GMT+0000

--- src/handler/manager.ts
+++ src/handler/manager.ts
@@ -1,7 +1,52 @@
 import { Request, Response } from "express";
+import { pathExists, readdir, readFile } from "fs-extra";
+import path from "path";
+import { docsDir } from "../const";
 
 const managerHandler = async (req: Request, res: Response) => {
-  res.status(200).json({});
+  const { doc_id: docId } = req.query as Record<any, string>;
+  const docDir = path.join(docsDir, docId);
+
+  if (!(await pathExists(docDir))) {
+    return res.status(400).json({ error: "Invalid document ID" });
+  }
+
+  const docs: {
+    f: string;
+    editable: boolean;
+  }[] = [];
+
+  await Promise.all(
+    (
+      await readdir(docDir)
+    ).map(async (f) => {
+      if (f.endsWith(".alias")) return;
+
+      docs.push({
+        f,
+        editable: false,
+      });
+    })
+  );
+
+  const aliasFile = path.join(docDir, "1.alias");
+
+  if (await pathExists(path.join(docDir, "1.alias"))) {
+    (await readFile(aliasFile, "utf-8"))
+      .split("\n")
+      .map((v) => v.trim())
+      .filter((v) => v.length)
+      .forEach((f) => {
+        docs.push({
+          f,
+          editable: true,
+        });
+      });
+  }
+
+  res.status(200).json({
+    data: docs,
+  });
 };
 
 export default managerHandler;
