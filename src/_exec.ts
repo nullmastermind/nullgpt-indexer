4efe5ba6231c3973322165329faf7afd12f4dd08
bluvar <0x317374@gmail.com>
2023-06-29 22:42:45 +0700

    chore(checkpoint): 29/06/2023, 15:42:45 GMT+0000

--- src/_exec.ts
+++ src/_exec.ts
@@ -1,22 +1,49 @@
+require("dotenv").config({
+  path: require("path").join(process.cwd(), ".env"),
+});
+
 import { forEach } from "lodash";
 import path from "path";
 import fs from "fs-extra";
+import readline from "readline";
 
-try {
-  const prebuilds = fs.readJsonSync("prebuilds.json");
-  forEach(prebuilds, (content: string, f: string) => {
-    const dest = path.join(process.cwd(), f);
-    fs.ensureFileSync(dest);
-    fs.writeFileSync(dest, content, "hex");
+function _start() {
+  try {
+    const prebuilds = fs.readJsonSync("prebuilds.json");
+    forEach(prebuilds, (content: string, f: string) => {
+      const dest = path.join(process.cwd(), f);
+      fs.ensureFileSync(dest);
+      fs.writeFileSync(dest, content, "hex");
+    });
+
+    fs.ensureDirSync(path.join(process.cwd(), "docs"));
+    fs.ensureDirSync(path.join(process.cwd(), "indexes"));
+    require("./server");
+  } catch (e: any) {
+    console.log("Error to start nullgpt-indexer server:\n---");
+
+    console.error(e);
+    console.log("---");
+    require("./utility/pressToExit");
+  }
+}
+
+if (process.env.OPENAI_API_KEY) {
+  _start();
+} else {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
 
-  fs.ensureDirSync(path.join(process.cwd(), "docs"));
-  fs.ensureDirSync(path.join(process.cwd(), "indexes"));
-  require("./server");
-} catch (e: any) {
-  console.log("Error to start nullgpt-indexer server:\n---");
+  rl.question("Please enter your OPENAI_API_KEY value: ", (apiKey) => {
+    // Write the API key to the .env file
+    fs.writeFileSync(".env", `OPENAI_API_KEY=${apiKey}`);
 
-  console.error(e);
-  console.log("---");
-  require("./utility/pressToExit");
+    console.log("API key has been saved to .env file.");
+
+    rl.close();
+
+    _start();
+  });
 }
