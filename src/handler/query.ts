915655801844733f3c95f01ab1abea1d4f8ba1ad
monkjiro <thenullmastermind@gmail.com>
2024-10-23 20:15:39 +0700

    refactor: Simplify query handling logic and remove unused code
    - Removed unused `scoreNormalizer2` import and related logic
    - Simplified `queryByVectorStore` function by removing strategy parameter and redundant conditions
    - Eliminated unnecessary code related to `codeVectorStore` and its queries
    - Streamlined the process of adding results to the data array

--- src/handler/query.ts
+++ src/handler/query.ts
@@ -4,48 +4,17 @@ import { encode } from 'gpt-3-encoder';
 import { Document } from 'langchain/document';
 import { cloneDeep, forEach, map } from 'lodash';
 
-import { getVectorStore, scoreNormalizer2 } from '../utility/common';
+import { getVectorStore } from '../utility/common';
 
-const queryByVectorStore = async (
-  req: Request,
-  vectorStore: FaissStore,
-  strategy: 'document' | 'code',
-) => {
-  const {
-    query,
-    k = {
-      document: 3,
-      code: 7,
-    }[strategy],
-    ignoreHashes = [],
-  } = req.body;
+const queryByVectorStore = async (req: Request, vectorStore: FaissStore) => {
+  const { query, ignoreHashes = [] } = req.body;
   const ignoredHashesSet = new Set<string>(ignoreHashes);
-  const results = await vectorStore.similaritySearchWithScore(query, k + ignoredHashesSet.size);
+  const results = await vectorStore.similaritySearchWithScore(query, 20);
   const data: [Document, number][] = [];
   const totalTokens = { current: 0 };
-  const lastScore = { current: -1 };
-  const includedSources = new Set<string>();
 
   forEach(results, (r) => {
-    // if (data.length >= k) return false;
-
-    // I don't know why the score is greater than 1.0, like 1.2, 1.6, etc.
-    // r[1] = Math.max(0.0, r[1] - 1.0);
-    // r[1] = 1.0 - r[1] / 2.0;
-    // console.log('score:', scoreNormalizer(r[1]), scoreNormalizer2(r[1]));
-
-    r[1] = scoreNormalizer2(r[1]);
-
-    // console.log('score:', r[1]);
-
-    // if (r[1] > maxScore) return false;
-
-    // https://stackoverflow.com/a/76700607
-    // if (!(r[1] >= 0.6 && r[1] <= 1.2)) return false;
-
-    if (lastScore.current === -1) {
-      lastScore.current = r[1];
-    }
+    r[1] = 0.0;
 
     if (ignoredHashesSet.has(r[0].metadata.hash)) {
       return;
@@ -56,28 +25,6 @@ const queryByVectorStore = async (
 
     totalTokens.current += encoded.length;
 
-    // if (totalTokens.current <= maxTokens) {
-    //   // const canAddC1 = r[1] <= includeAllIfKLessThanScore;
-    //   const canAddC1 = false;
-    //   // const canAddC2 = r[1] - lastScore.current <= scoreChangeThreshold;
-    //   const canAddC2 = false;
-    //   const canAddC3 = includedSources.has(r[0].metadata.source);
-    //   const canAdd = canAddC1 || canAddC2 || canAddC3;
-    //
-    //   if (!canAdd) {
-    //     if (data.length >= k) {
-    //       return false;
-    //     }
-    //   }
-    //
-    //   includedSources.add(r[0].metadata.source);
-    //   data.push(r);
-    // } else {
-    //   totalTokens.current -= encoded.length;
-    //   return false;
-    // }
-
-    includedSources.add(r[0].metadata.source);
     data.push(r);
   });
 
@@ -104,7 +51,7 @@ const queryByVectorStore = async (
     });
     dataBySource[source] = data.map((doc) => {
       if (doc?.[0]?.metadata) {
-        doc[0].metadata.summary = strategy === 'document';
+        doc[0].metadata.summary = true;
       }
       return doc;
     });
@@ -118,7 +65,6 @@ const queryByVectorStore = async (
 
 const queryHandler = async (req: Request, res: Response) => {
   const { doc_id: docId, api_key: apiKey, query = '' } = req.body;
-  const codeVectorStore = await getVectorStore(docId + '+code', docId, apiKey);
   const docVectorStore = await getVectorStore(docId, docId, apiKey);
 
   const queries: any[] = [];
@@ -127,17 +73,11 @@ const queryHandler = async (req: Request, res: Response) => {
     const req1 = cloneDeep(req);
     req1.body.query = args;
     req1.body.k = Math.min(req1.body.k, 3);
-    queries.push(queryByVectorStore(req1, docVectorStore, 'document'));
-    queries.push(queryByVectorStore(req1, codeVectorStore, 'code'));
+    queries.push(queryByVectorStore(req1, docVectorStore));
     return substring;
   });
 
-  if (queries.length) {
-    req.body.k = Math.min(req.body.k, 3);
-  }
-
-  queries.push(queryByVectorStore(req, docVectorStore, 'document'));
-  queries.push(queryByVectorStore(req, codeVectorStore, 'code'));
+  queries.push(queryByVectorStore(req, docVectorStore));
 
   const queryResults = await Promise.all(queries);
   const resData: any[] = [];
